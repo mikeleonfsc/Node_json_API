ba9f5c5775e27e6db3d9a897aa4f6db7050dd025
Mike Leon <mleon@follett.com>
2017-06-05 21:07:53 -0500

    need to add access control header for angular to access

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var express = require('express')
 var app = express()
 
 app.get('/movies', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*') 
     res.json(
         [
             { name: "Star Wars.", rating: 1, watchAgain: false },
-            { name: "Star Wars 2", rating: 1, watchAgain: false }
+            { name: "Star Wars 2", rating: 1, watchAgain: false },
+            { name: "Bull Durham", rating: 3, watchAgain: false }
 
         ]
     )
